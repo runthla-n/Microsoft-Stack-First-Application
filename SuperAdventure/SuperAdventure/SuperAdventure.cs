28754788fb608c880d6650f402afa9d0568799d8
runthla-n <sharma.nikhil24@yahoo.in>
2018-08-23 23:54:05 +0530

    Changes : 05.1 – Creating objects from classes

--- SuperAdventure/SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure/SuperAdventure.cs
@@ -1,22 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Engine;
+
 namespace SuperAdventure
 {
-	static class SuperAdventure
+	public partial class SuperAdventure : Form
 	{
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		[STAThread]
-		static void Main()
+		private Player _player;
+
+		public SuperAdventure()
 		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
+			InitializeComponent();
+
+			_player = new Player();
+
+			_player.CurrentHitPoints = 10;
+			_player.MaximumHitPoints = 10;
+			_player.Gold = 20;
+			_player.ExperiencePoints = 0;
+			_player.Level = 1;
+
+			lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+			lblGold.Text = _player.Gold.ToString();
+			lblExperience.Text = _player.ExperiencePoints.ToString();
+			lblLevel.Text = _player.Level.ToString();
 		}
 	}
 }
